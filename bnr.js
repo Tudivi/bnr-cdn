e6f8c6cab9e44394be8c6645051c480ac25572da
Tudivi <tudivimedia@gmail.com>
2025-03-27 00:43:26 +0200

    Update bnr.js
    Delete all + compress in js

--- bnr.js
+++ bnr.js
@@ -1,14 +1,29 @@
-
 function modifyText() {
     var tudiviLink = document.querySelector('body > a:last-child');
     if (tudiviLink) {
         var p = tudiviLink.querySelector('p');
         if (p && p.textContent.includes('Made With TUDIVI EDITOR')) {
-            p.textContent = 'REALIZAT CU TUDIVI EDITOR'; 
-            return true; 
+            p.textContent = 'REALIZAT CU TUDIVI EDITOR';
         }
+
+        tudiviLink.style.position = "static";
+        tudiviLink.style.padding = "10px";
+        tudiviLink.style.justifyContent = "center";
+        tudiviLink.style.width = "100%";
+
+        var img = tudiviLink.querySelector("img");
+        if (img) {
+            img.style.width = "18px";
+            img.style.height = "18px";
+        }
+
+        if (p) {
+            p.style.fontSize = "14px";
+        }
+
+        return true;
     }
-    return false; 
+    return false;
 }
 
 var observer = new MutationObserver(function(mutations) {
@@ -20,3 +35,5 @@ var observer = new MutationObserver(function(mutations) {
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
+
+modifyText();
